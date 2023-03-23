1f1ab8b329dfa1b889ca4c865cec72ff8879e776
EmilyBernard <bernam2000@hotmail.com>
2023-03-23 13:32:24 -0400

    corrected db name in ADD function in database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,10 +18,10 @@ export const putDb = async (content) => {
 console.log('Add to the database');
 
 // Create a connection to the database database and version we want to use.
-const contactDb = await openDB('jate', 1);
+const jateDb = await openDB('jate', 1);
 
 // Create a new transaction and specify the database and data privileges.
-const tx = contactDb.transaction('jate', 'readwrite');
+const tx = jateDb.transaction('jate', 'readwrite');
 
 // Open up the desired object store.
 const store = tx.objectStore('jate');
