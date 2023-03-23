59d4cbee639c853087d38e6a686a98b1c0822a62
EmilyBernard <bernam2000@hotmail.com>
2023-03-23 13:30:26 -0400

    added ADD logic to database.js file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // DONE Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// DONE Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
